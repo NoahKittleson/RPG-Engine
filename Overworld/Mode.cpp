f4227de463c90addef0932e2d5298cbb4ba5b6af
Noah Kittleson <noahkittleson@gmail.com>
2017-03-12 16:49:23 -0700

    Added Mode base class for use of state pattern without having to add anything new to state stack

---
+++ Overworld/Mode.cpp
@@ -0,0 +1,9 @@
+//
+//  Mode.cpp
+//  Overworld
+//
+//  Created by Noah Kittleson on 3/10/17.
+//  Copyright © 2017 Noah. All rights reserved.
+//
+
+#include "Mode.hpp"
---
+++ Overworld/Mode.hpp
@@ -0,0 +1,14 @@
+//
+//  Mode.hpp
+//  Overworld
+//
+//  Created by Noah Kittleson on 3/10/17.
+//  Copyright © 2017 Noah. All rights reserved.
+//
+
+#ifndef Mode_hpp
+#define Mode_hpp
+
+#include <stdio.h>
+
+#endif /* Mode_hpp */
