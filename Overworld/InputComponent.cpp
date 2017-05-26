ffe05f2d2fa9698f8b281bba65f170443159bffa
Noah Kittleson <noahkittleson@gmail.com>
2017-05-26 16:09:31 -0700

    made generic InputComponent

---
+++ Overworld/InputComponent.cpp
@@ -0,0 +1,14 @@
+//
+//  InputComponent.cpp
+//  Overworld
+//
+//  Created by Noah Kittleson on 5/26/17.
+//  Copyright © 2017 Noah. All rights reserved.
+//
+
+#include "InputComponent.hpp"
+
+void InputComponent::move(float elapsed) {
+	return;
+}
+
---
+++ Overworld/InputComponent.hpp
@@ -0,0 +1,21 @@
+//
+//  InputComponent.hpp
+//  Overworld
+//
+//  Created by Noah Kittleson on 5/26/17.
+//  Copyright © 2017 Noah. All rights reserved.
+//
+
+#pragma once
+#include "PrefixHeader.pch"
+
+class InputComponent
+{
+public:
+	virtual ~InputComponent() {}
+	InputComponent () {}
+	virtual void move(float elapsed);
+	
+private:
+	
+};
