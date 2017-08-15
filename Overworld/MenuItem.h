b48a0f04d314c92fcb62d6c814198ce95d73966c
Noah Kittleson <noahkittleson@gmail.com>
2017-08-14 23:32:13 -0700

    started writing new abstrct class for MenuItems that will hopefully implement a easier to follow and more modular menu architecture

--- Overworld/MenuItem.h
+++ Overworld/MenuItem.h
@@ -27,29 +27,61 @@ private:
     
 };
 
-class NewMenuOption : public sf::Text
+class NewMenuItem
 {
-    MenuItem::Action action;
+public:
+	NewMenuItem(const sf::Font& font, std::string&& string)
+	{
+		text.setString(string);
+		text.setFont(font);
+	};
+	
+	virtual void draw(sf::RenderWindow &rw) = 0;
+	virtual void update(float elapsed) = 0;
+	virtual void handleInput(sf::RenderWindow& rw);
+	virtual void select() = 0;						//select is for highlighting current option, activate for executing it
+	virtual void activate() = 0;
+protected:
+	sf::Text text;
+	bool selected = false;
+	bool active = false;
 };
 
-//template <class Type>
-//class MenuOption
-//{
-//    Type var;
-//};
-
-//enum MenuStates : pickState, basic attack, 
-
-
-
-//reasons for keeping menuitem...
-//-it's simple
-//-it works totaly fine
-//
-//
-//
-
-//reasons for deleting MenuItem
-//-it's barely anything more than sf::text (put it in with menu)
-//-it's bloating the filelist
-//
+class MenuItemContainer : public NewMenuItem {
+public:
+	MenuItemContainer(const sf::Font& font, std::string&& string)
+	: NewMenuItem(font, std::move(string))
+	{
+		
+	};
+	
+	void draw(sf::RenderWindow &rw) {
+		rw.draw(text);
+		//draw selected marker?
+	}
+	
+	void update(float elapsed){
+		//nothing yet?
+	}
+	
+	void handleInput(sf::RenderWindow& rw) {
+		if (!active) {
+			return;
+		}
+		sf::Event event;
+		while (rw.pollEvent(event)) {
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down) {
+				++options;
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up) {
+				--options;
+			}
+			if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::X) {
+				options.get().activate();
+			}
+		}
+	}
+	
+private:
+	IterVector<NewMenuItem> options;
+};
