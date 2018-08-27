56114e31a779d3adc54d0808aa54112364fae65b
Noah Kittleson <noahkittleson@gmail.com>
2018-08-26 20:40:27 -0700

    adding new StaggeredAnimation class

---
+++ Overworld/StaggeredAnimation.cpp
@@ -0,0 +1,9 @@
+//
+//  StaggeredAnimation.cpp
+//  Overworld
+//
+//  Created by Noah Kittleson on 8/26/18.
+//  Copyright © 2018 Noah. All rights reserved.
+//
+
+#include "StaggeredAnimation.hpp"
---
+++ Overworld/StaggeredAnimation.hpp
@@ -0,0 +1,21 @@
+//
+//  StaggeredAnimation.hpp
+//  Overworld
+//
+//  Created by Noah Kittleson on 8/26/18.
+//  Copyright © 2018 Noah. All rights reserved.
+//
+
+#pragma once
+#include "AnimatedComponent.hpp"
+#include "IterVector.hpp"
+
+class StaggeredAnimation : public AnimatedComponent {
+public:
+	StaggeredAnimation(const sf::Texture&, sf::Vector2f, IterVector<float> frameLengths, sf::Vector2i frame);
+	virtual void update(float elapsed) override;
+	std::unique_ptr<GraphicsComponent> getCopy() override;
+	
+protected:
+	const IterVector<float> frameLengths;
+};
